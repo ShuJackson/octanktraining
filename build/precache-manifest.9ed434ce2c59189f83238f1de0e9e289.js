15a7cb74c84ce7ae87a700ea6fa934a71d00f794
Jackson <shujacks@3c22fbbebcec.ant.amazon.com>
2020-10-15 17:12:09 -0400

    start new

---
+++ build/precache-manifest.9ed434ce2c59189f83238f1de0e9e289.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0795ffedad380ad80d3897bb27415dc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2810373263a2ae1f7eaa",
+    "url": "/static/css/2.22838211.chunk.css"
+  },
+  {
+    "revision": "d972fc718e7fd5267807",
+    "url": "/static/css/main.ad9a7ae5.chunk.css"
+  },
+  {
+    "revision": "2810373263a2ae1f7eaa",
+    "url": "/static/js/2.8d300597.chunk.js"
+  },
+  {
+    "revision": "d34204dc1e9fefb3014117f2fc898555",
+    "url": "/static/js/2.8d300597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d972fc718e7fd5267807",
+    "url": "/static/js/main.ddca7c7b.chunk.js"
+  },
+  {
+    "revision": "cb2279d7223af0095d43",
+    "url": "/static/js/runtime-main.054b8cd5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
